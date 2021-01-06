87b7f5d3729dfe1e5f9af533524d07dd1e614fa2
Alfio Martini <alfio.martini@gmail.com>
2021-01-06 18:49:20 -0300

    Added basic routing (1st version)

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,41 @@
 import React from 'react';
-// import Accordion from './components/Accordion';
+import Accordion from './components/Accordion';
 import Dropdown from './components/Dropdown';
-// import SearchBox from './components/SearchBox';
-// import Translator from './components/Translator';
+import SearchBox from './components/SearchBox';
+import Translator from './components/Translator';
+
+const showAccordion = () => {
+  if (window.location.pathname === '/'){
+    return <Accordion />
+  } 
+}
+
+const showSearchBox = () => {
+  if (window.location.pathname === '/wiki'){
+    return <SearchBox />
+  } 
+}
+
+const showDropdown = () => {
+  if (window.location.pathname === '/dropdown'){
+    return <Dropdown />
+  } 
+}
+
+const showTranslator = () => {
+  if (window.location.pathname === '/translate'){
+    return <Translator />
+  } 
+}
 
 const App = () => {
   return(
-    // <Accordion />
-    // <SearchBox />
-    <Dropdown />
-    // <Translator />
+    <div>
+      {showAccordion()}
+      {showSearchBox()}
+      {showDropdown()}
+      {showTranslator()}
+    </div>
   )
 }
 
